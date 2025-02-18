37359ca5ab8ee99178a1000ad9930c087f9b421d
manysleeplover1 <manysleeplover@bk.ru>
2025-02-18 18:52:44 +0300

    Lab1 - init commit

---
+++ Lab1/server-side-application/src/main/kotlin/ru/romanov/stankin/server_side_application/ServerSideApplication.kt
@@ -0,0 +1,11 @@
+package ru.romanov.stankin.server_side_application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerSideApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServerSideApplication>(*args)
+}
---
+++ Lab1/server-side-application/src/test/kotlin/ru/romanov/stankin/server_side_application/ServerSideApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.romanov.stankin.server_side_application
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerSideApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
